b0b19025fd8c60674c632f697a0dbce794fd64bc
vk0027 <123943655+vk0027@users.noreply.github.com>
2023-02-07 08:02:49 +0530

    I have written the prime number code

---
+++ prime.js
@@ -0,0 +1,9 @@
+#Prime Problem-
+                let number=5;
+                let factor=0;
+                for(let i=1;i<=number;i++){
+                if(number%i==1){
+                 factor++;
+                 }
+                }
+             (factor==2)?console.log("it is prime"):console.log("it is not a prime number");
